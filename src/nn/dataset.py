2bcd73300522b2e191aa2bb56db05fbecbe8df82
pandov <ipandov17@gmail.com>
2020-07-11 22:04:55 +0300

    Split to train and valid transforms

--- src/nn/dataset.py
+++ src/nn/dataset.py
@@ -29,15 +29,6 @@ def train_transform():
 
 def valid_transform():
     return transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-
-def test_transform():
-    return transforms.Compose([
         transforms.Grayscale(),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
