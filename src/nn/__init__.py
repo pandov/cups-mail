31ce734687781a1206c86f98e60bc2703a52b171
pandov <ipandov17@gmail.com>
2020-07-12 17:54:39 +0300

    Apply components approach

--- src/nn/__init__.py
+++ src/nn/__init__.py
@@ -5,9 +5,18 @@ from torch.nn import CrossEntropyLoss
 from .loss import DiceLoss
 from .dataset import BACTERIA
 
-def get_scheduler(n, optimizer):
+def get_classification_model(n, num_classes=6):
     if n == 1:
-        return torch.optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
+        model = torchvision.models.resnet50(pretrained=True)
+        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+    if n == 2:
+        model = torchvision.models.vgg16_bn(pretrained=True)
+        model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, num_classes)
+    return model
+
+def get_segmentation_model(n):
+    if n == 1:
+        return torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1, init_features=32, pretrained=True)
 
 def get_optimizer(n, model):
     if n == 1:
@@ -15,27 +24,20 @@ def get_optimizer(n, model):
     elif n == 2:
         return torch.optim.RMSprop(model.parameters(), lr=1e-2)
 
-def get_segmentation_components(n):
+def get_scheduler(n, optimizer):
     if n == 1:
-        model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=1, init_features=32, pretrained=True)
-        optimizer = get_optimizer(1, model)
-        scheduler = get_scheduler(1, optimizer)
+        return torch.optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
 
-    model.train()
+def get_segmentation_components(m, o, s):
+    model = get_segmentation_model(m)
+    optimizer = get_optimizer(o, model)
+    scheduler = get_scheduler(s, optimizer)
     return model, optimizer, scheduler
 
-def get_classification_components(n, num_classes=6):
-    if n == 1:
-        model = torchvision.models.resnet50(pretrained=True)
-        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-        optimizer = get_optimizer(1, model)
-        scheduler = get_scheduler(1, optimizer)
-    elif n == 2:
-        model = torchvision.models.vgg16_bn(pretrained=True)
-        optimizer = get_optimizer(1, model)
-        scheduler = get_scheduler(1, optimizer)
-
-    model.train()
+def get_classification_components(m, o, s):
+    model = get_classification_model(m)
+    optimizer = get_optimizer(o, model)
+    scheduler = get_scheduler(s, optimizer)
     return model, optimizer, scheduler
 
 def get_class_names():
