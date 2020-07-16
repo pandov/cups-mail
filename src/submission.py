4e1690c3d2709183fd3f83989c523d7168eb0eb6
pandov <ipandov17@gmail.com>
2020-07-17 02:19:19 +0300

    Remove negative

--- src/submission.py
+++ src/submission.py
@@ -32,7 +32,7 @@ def get_test_transform():
     return transforms.Compose([
         transforms.Grayscale(3),
         transforms.ToTensor(),
-        transforms.Lambda(negative_normalize),
+        # transforms.Lambda(negative_normalize),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
