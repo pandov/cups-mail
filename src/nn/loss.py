34a414518ce96db004b2a268ee91fde3ebdfbfc2
pandov <ipandov17@gmail.com>
2020-07-17 02:31:05 +0300

    Stage 2

--- src/nn/loss.py
+++ src/nn/loss.py
@@ -11,5 +11,6 @@ class DiceLoss(Module):
         y_pred = y_pred[:, 0].contiguous().view(-1)
         y_true = y_true[:, 0].contiguous().view(-1)
         intersection = (y_pred * y_true).sum()
-        dsc = (2. * intersection + self.smooth) / (y_pred.sum() + y_true.sum() + self.smooth)
+        union = y_pred.sum() + y_true.sum()
+        dsc = (2. * intersection + self.smooth) / (union + self.smooth)
         return 1. - dsc
